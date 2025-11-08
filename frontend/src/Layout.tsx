898117bad07fb3f853a151b9722c0cf60eb945b9
012e <huyphmnhat@gmail.com>
2025-11-08 18:26:10 +0700

    minor changes

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -2,17 +2,24 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { Outlet } from "react-router";
 import { ThemeProvider } from "@/components/theme-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 export default function Layout() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <SidebarProvider>
-        <AppSidebar />
-        <main className="flex flex-col w-full h-screen bg-background">
-          <SidebarTrigger />
-          <Outlet />
-        </main>
-      </SidebarProvider>
+      <QueryClientProvider client={queryClient}>
+        <SidebarProvider>
+          <AppSidebar />
+          <ReactQueryDevtools initialIsOpen={false} />
+          <main className="flex flex-col w-full h-screen bg-background">
+            <SidebarTrigger />
+            <Outlet />
+          </main>
+        </SidebarProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
