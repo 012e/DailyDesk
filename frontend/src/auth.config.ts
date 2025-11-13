29ee8013b010f667c07316f374c2c7b07dff2c31
loihnt05 <23520869@gm.uit.edu.vn>
2025-11-13 14:43:48 +0700

    change auth0

---
+++ frontend/src/auth.config.ts
@@ -0,0 +1,12 @@
+interface AuthConfig {
+  domain: string;
+  clientId: string;
+  audience?: string;
+}
+
+
+export const authConfig: AuthConfig = {
+  domain: import.meta.env.VITE_AUTH0_DOMAIN as string,
+  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID as string,
+  audience: import.meta.env.VITE_AUTH0_API_AUDIENCE as string,
+};
--- frontend/src/pages/sign-in.tsx
+++ frontend/src/pages/sign-in.tsx
@@ -1,11 +1,19 @@
-import { SignIn } from '@clerk/react-router';
+import LoginButton from "@/components/login-button";
+import LogoutButton from "@/components/logout";
+import Profile from "@/components/profile";
+import { useAuth0 } from "@auth0/auth0-react";
 export default function SignInPage() {
+  const { isAuthenticated } = useAuth0();
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black">
-      <div className="w-full max-w-md p-8">
-        <h1 className="text-3xl font-bold text-center mb-8 text-white">Well cumback</h1>
-          <SignIn signUpUrl='/sign-up'/>
-      </div>
+    <div>
+      {!isAuthenticated ? (
+        <LoginButton />
+      ) : (
+        <div>
+          <LogoutButton />
+          <Profile />
+        </div>
+      )}
     </div>
   );
 }
