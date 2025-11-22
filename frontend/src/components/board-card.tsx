7ffe5b63b90df9a1b9f3c9b4dde8b97e84bd0281
012e <huyphmnhat@gmail.com>
2025-11-23 01:43:16 +0700

    --wip--

--- frontend/src/components/board-card.tsx
+++ frontend/src/components/board-card.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useTheme } from "@/components/theme-provider";
 import type { HTMLAttributes } from "react";
+import { Link } from "react-router";
 
 export interface BoardCardProps {
   id: string;
@@ -15,7 +16,6 @@ export function BoardCard({
   backgroundUrl,
   backgroundColor,
 }: BoardCardProps) {
-  const theme = useTheme();
   let backgroundStyle: HTMLAttributes<HTMLDivElement>["style"];
   if (backgroundUrl) {
     backgroundStyle = {
@@ -26,27 +26,29 @@ export function BoardCard({
     backgroundStyle = { backgroundColor };
   }
   return (
-    <Card
-      key={id}
-      className="overflow-hidden relative gap-0 pt-4 pb-14 bg-center bg-no-repeat bg-cover border-0 transition-all cursor-pointer hover:shadow-md dark:hover:shadow-md-white hover:scale-[1.02]"
-      style={backgroundStyle}
-    >
-      <div className="absolute inset-0 mb-16 pointer-events-none backdrop-blur-sm dark:bg-black/25" />
+    <Link to={`/board/${id}`}>
+      <Card
+        key={id}
+        className="overflow-hidden relative gap-0 pt-4 pb-14 bg-center bg-no-repeat bg-cover border-0 transition-all cursor-pointer hover:shadow-md dark:hover:shadow-md-white hover:scale-[1.02]"
+        style={backgroundStyle}
+      >
+        <div className="absolute inset-0 mb-16 pointer-events-none backdrop-blur-sm dark:bg-black/25" />
 
-      <CardHeader className="flex justify-between items-center p-4 py-0 z-5">
-        <CardTitle className="text-md font-medium overflow-hidden text-ellipsis max-w-[80%] z-10 text-">
-          {title}
-        </CardTitle>
-        <div
-          className="top-4 right-4 w-4 h-4 rounded-full pointer-events-none"
-          style={backgroundStyle}
-        />
-      </CardHeader>
-      <CardContent className="z-10 p-4 pt-0">
-        <p className="text-sm dark:text-gray-200 text-muted-foreground">
-          Click to open board
-        </p>
-      </CardContent>
-    </Card>
+        <CardHeader className="flex justify-between items-center p-4 py-0 z-5">
+          <CardTitle className="text-md font-medium overflow-hidden text-ellipsis max-w-[80%] z-10 text-">
+            {title}
+          </CardTitle>
+          <div
+            className="top-4 right-4 w-4 h-4 rounded-full pointer-events-none"
+            style={backgroundStyle}
+          />
+        </CardHeader>
+        <CardContent className="z-10 p-4 pt-0">
+          <p className="text-sm dark:text-gray-200 text-muted-foreground">
+            Click to open board
+          </p>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
