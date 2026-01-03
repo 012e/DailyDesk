8e00485a45311df27cf109c52a940a9f3ee4f0b4
012e <huyphmnhat@gmail.com>
2026-01-03 18:00:46 +0700

    fix migrations

--- backend/drizzle/0000_lazy_misty_knight.sql
+++ backend/drizzle/0000_lazy_misty_knight.sql
@@ -1,3 +1,15 @@
+CREATE TABLE `board_members` (
+	`id` text PRIMARY KEY NOT NULL,
+	`board_id` text NOT NULL,
+	`user_id` text NOT NULL,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	`avatar` text,
+	`role` text DEFAULT 'member' NOT NULL,
+	`added_at` integer NOT NULL,
+	FOREIGN KEY (`board_id`) REFERENCES `boards`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
 CREATE TABLE `boards` (
 	`id` text PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
@@ -7,9 +19,26 @@ CREATE TABLE `boards` (
 	`background_color` text
 );
 --> statement-breakpoint
+CREATE TABLE `card_labels` (
+	`id` text PRIMARY KEY NOT NULL,
+	`card_id` text NOT NULL,
+	`label_id` text NOT NULL,
+	FOREIGN KEY (`card_id`) REFERENCES `cards`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`label_id`) REFERENCES `labels`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `card_members` (
+	`id` text PRIMARY KEY NOT NULL,
+	`card_id` text NOT NULL,
+	`member_id` text NOT NULL,
+	FOREIGN KEY (`card_id`) REFERENCES `cards`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`member_id`) REFERENCES `board_members`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
 CREATE TABLE `cards` (
 	`id` text PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
+	`description` text,
 	`order` integer NOT NULL,
 	`cover_url` text,
 	`cover_public_id` text,
@@ -33,7 +62,10 @@ CREATE TABLE `checklist_items` (
 --> statement-breakpoint
 CREATE TABLE `labels` (
 	`id` text PRIMARY KEY NOT NULL,
-	`name` text NOT NULL
+	`name` text NOT NULL,
+	`color` text NOT NULL,
+	`board_id` text NOT NULL,
+	FOREIGN KEY (`board_id`) REFERENCES `boards`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
 CREATE TABLE `lists` (
